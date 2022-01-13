3e7e2c5f1775a99ca39b86b79b72b024390e0e86
khauswirth3123 <kendall.hauswirth@gmail.com>
2022-01-13 11:04:34 -0700

    Added hello world

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,19 @@
+print("Helloworld")
+print("Lookimcoding")
+x = 6
+print("x = " , x)
+x += 9
+print("x = " , x)
+y = 15
+if x > y:
+    print("x > 14")
+elif x == y :
+    print("crikey")
+else:
+    print("<",y)
+
+#this is my new function
+def my_function(fname,age):
+    print(fname + " Hauswirth " + "is " + str(age))
+
+my_function("Kendall", 18)
